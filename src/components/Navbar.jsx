6fc3bb554d6a6db7d284b0276733c9572d59c533
Zaibbee69 <jahanzaib12569@gmail.com>
2026-02-13 00:32:46 +0500

    Custom Themes added Navbar Prototype

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,26 @@
+import { Grid, Flex, Container, Heading, Box, Text } from "@radix-ui/themes";
+
 export default function Navbar() {
-  return <nav className="">Navbar</nav>;
+  return (
+    <nav>
+      <Grid columns="1" rows="1fr, 2fr">
+        <Container className="bg-brand-red relative" color="grey" p={"24px"}>
+          <Flex justify="center" align="center">
+            <Heading as="h1" size="9">
+              Store
+            </Heading>
+          </Flex>
+          <Box className="absolute right-4 top-1/2 -translate-y-1/2">
+            <Flex gap="3">
+              <Text>Icon 1</Text>
+              <Text>Icon 2</Text>
+            </Flex>
+          </Box>
+        </Container>
+        <Container className="bg-brand-black">
+          <Text>YEAH</Text>
+        </Container>
+      </Grid>
+    </nav>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 import "./index.css";
 
 import App from "./App.jsx";
@@ -9,7 +11,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       {/* Add Context Provider here */}
-      <App />
+      <Theme appearance="dark" accentColor="red" grayColor="gray">
+        <App />
+      </Theme>
     </BrowserRouter>
   </StrictMode>,
 );
