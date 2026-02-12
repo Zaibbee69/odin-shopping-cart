35b68f626ff055cf779d8f97f1272da9a044843d
Zaibbee69 <jahanzaib12569@gmail.com>
2026-02-13 01:02:06 +0500

    Added React Bits Nav, GSAP, Lucide React Icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,49 @@
-import { Grid, Flex, Container, Heading, Box, Text } from "@radix-ui/themes";
+import { Flex, Container, Heading, Box, Text } from "@radix-ui/themes";
+import { Clapperboard } from "lucide-react";
+import PillNav from "./PillNav";
 
 export default function Navbar() {
   return (
     <nav>
-      <Grid columns="1" rows="1fr, 2fr">
-        <Container className="bg-brand-red relative" color="grey" p={"24px"}>
-          <Flex justify="center" align="center">
-            <Heading as="h1" size="9">
-              Store
-            </Heading>
+      <Container className="bg-brand-red relative" color="grey" p={"24px"}>
+        <Flex justify="center" align="center">
+          <Heading as="h1" size="9">
+            Store
+          </Heading>
+        </Flex>
+        <Box className="absolute right-4 top-1/2 -translate-y-1/2">
+          <Flex gap="3">
+            <Text>Icon 1</Text>
+            <Text>Icon 2</Text>
           </Flex>
-          <Box className="absolute right-4 top-1/2 -translate-y-1/2">
-            <Flex gap="3">
-              <Text>Icon 1</Text>
-              <Text>Icon 2</Text>
-            </Flex>
-          </Box>
-        </Container>
-        <Container className="bg-brand-black">
-          <Text>YEAH</Text>
-        </Container>
-      </Grid>
+        </Box>
+      </Container>
+      <Flex
+        justify={"center"}
+        align={"center"}
+        className="bg-brand-black"
+        p={"8px"}
+      >
+        <PillNav
+          logo={<Clapperboard />}
+          logoAlt="Company Logo"
+          items={[
+            { label: "Home", href: "/" },
+            { label: "About", href: "/about" },
+            { label: "Services", href: "/services" },
+            { label: "Contact", href: "/contact" },
+          ]}
+          activeHref="/"
+          className="custom-nav"
+          ease="power2.easeOut"
+          baseColor="#000000"
+          pillColor="#ffffff"
+          hoveredPillTextColor="#ffffff"
+          pillTextColor="#000000"
+          theme="dark"
+          initialLoadAnimation={false}
+        />
+      </Flex>
     </nav>
   );
 }
