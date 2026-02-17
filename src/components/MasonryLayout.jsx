09f56d526e22f86d92309c1687c95abc76397ffb
Zaibbee69 <jahanzaib12569@gmail.com>
2026-02-18 01:10:31 +0500

    Masonry Layout Component Prototype done

---
+++ src/components/MasonryLayout.jsx
@@ -0,0 +1,63 @@
+import { useTrendingMovies } from "../hooks/useTrendingMovies";
+
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import { Card, Text, Box, Heading } from "@radix-ui/themes";
+
+export default function MasonryLayout() {
+  const { movies, isLoading, isError } = useTrendingMovies();
+  if (isLoading)
+    return <p className="text-white text-center py-20">Loading...</p>;
+
+  if (isError)
+    return (
+      <p className="text-red-500 text-center py-20">Failed to load movies</p>
+    );
+
+  return (
+    <div className="px-6 md:px-12 lg:px-20 py-12">
+      <ResponsiveMasonry
+        columnsCountBreakPoints={{ 350: 1, 750: 2, 1024: 3, 1280: 4 }}
+      >
+        <Masonry gutter="20px">
+          {movies.map((movie) => {
+            const isWide = movie.id % 10 < 3;
+            const imagePath = isWide ? movie.backdrop_path : movie.poster_path;
+            const size = isWide ? "w780" : "w342";
+
+            if (!imagePath) return null;
+
+            return (
+              <Card
+                key={movie.id}
+                asChild
+                className="group overflow-hidden bg-neutral-900 border border-neutral-800 rounded-xl transition-all duration-300 hover:shadow-2xl hover:shadow-black/40"
+              >
+                <a href="#" className="block relative">
+                  <img
+                    src={`https://image.tmdb.org/t/p/${size}${imagePath}`}
+                    alt={movie.title}
+                    className="w-full h-auto object-cover transition-transform duration-500 group-hover:scale-105"
+                  />
+
+                  <Box className="absolute inset-0 bg-linear-to-t from-black/80 via-black/30 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+
+                  <Box className="absolute bottom-0 p-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                    <Heading
+                      as="p"
+                      className="text-white font-semibold text-sm md:text-base"
+                    >
+                      {movie.title}
+                    </Heading>
+                  </Box>
+                  <Box>
+                    <Text>Ratings: {movie.vote_average}</Text>
+                  </Box>
+                </a>
+              </Card>
+            );
+          })}
+        </Masonry>
+      </ResponsiveMasonry>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,54 +1,18 @@
 import { useTrendingMovies } from "../hooks/useTrendingMovies";
-import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
-import { Card, Inset, Text } from "@radix-ui/themes";
+import MasonryLayout from "../components/MasonryLayout";
 
 export default function Home() {
   const { movies, isLoading, isError } = useTrendingMovies();
 
-  if (isLoading) return <p>Loading...</p>;
-  if (isError) return <p>Failed to load movies</p>;
+  if (isLoading)
+    return <p className="text-white text-center py-20">Loading...</p>;
 
-  return (
-    <ResponsiveMasonry
-      columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
-      gutterBreakpoints={{ 350: "12px", 750: "16px", 900: "24px" }}
-    >
-      <Masonry columnsCount={5} gutter="12px">
-        {movies.map((movie, index) => {
-          const isWide = index % 3 === 0;
+  if (isError)
+    return (
+      <p className="text-red-500 text-center py-20">Failed to load movies</p>
+    );
 
-          const imagePath = isWide ? movie.backdrop_path : movie.poster_path;
+  console.log(movies);
 
-          const size = isWide ? "w780" : "w342";
-
-          return (
-            <Card key={movie.id} asChild>
-              <a href="#">
-                <Inset clip="padding-box" side="top" pb="current">
-                  <img
-                    src={`https://image.tmdb.org/t/p/${size}${imagePath}`}
-                    alt={movie.title}
-                    className="w-full rounded-lg"
-                    // style={{
-                    //   display: "block",
-                    //   objectFit: "cover",
-                    //   width: "100%",
-                    //   height: 140,
-                    //   backgroundColor: "var(--gray-5)",
-                    // }}
-                  />
-                </Inset>
-                <Text as="p">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Voluptate, nisi corporis sit quod minus natus aut debitis
-                  necessitatibus accusamus rem hic fuga libero tempora nobis
-                  consequuntur atque blanditiis? Veniam, obcaecati!
-                </Text>
-              </a>
-            </Card>
-          );
-        })}
-      </Masonry>
-    </ResponsiveMasonry>
-  );
+  return <MasonryLayout />;
 }
